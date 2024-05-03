d3e0b15fccaaf00d3d7e39465950b7bc78d96d92
Yago Souza <138893220+Yago2023@users.noreply.github.com>
2024-05-02 21:00:14 -0300

    Etapa inicial do projeto

--- face_recognition.py
+++ face_recognition.py
@@ -1,9 +1,9 @@
 import pathlib
 import cv2
 
-cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
+cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml" 
 
-clf = cv2.CascadeClassifier(str(cascade_path))
+clf = cv2.CascadeClassifier(str(cascade_path)) # cascata Haar é para tarefas como detecção de rostos. 
 
 camera = cv2.VideoCapture(0)
 
