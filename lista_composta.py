66bec213b2da0b86864e4531a7d686340cddb2e9
Yago <yagoelorena@icloud.com>
2024-06-19 22:37:33 -0300

    aula lista composta

---
+++ lista_composta.py
@@ -0,0 +1,12 @@
+alunos = list()
+alunos.append ('yago')
+alunos.append  ('18')
+sala = list()
+sala.append(alunos[:])
+alunos[0] = 'maria'
+alunos [1] = '16'
+print(sala)
+sala.append(alunos[:])
+print(sala)
+
+
