d9d5688c80e689cfdfe6f76495c8fca3f176b22e
Yago <yagoelorena@icloud.com>
2024-07-17 22:05:06 -0300

    Exercicio Dict - 90

---
+++ ex.90.py
@@ -0,0 +1,8 @@
+situacao = dict()
+situacao ['nome'] = str(input('Digite o nome do aluno: '))
+situacao ['nota'] = int(input(f'Digite a media de {situacao["nome"]} : '))
+print(f'Média é igual a de {situacao["nome"]} é {situacao["nota"]} ')
+if situacao['nota']>=6:
+    print(f'Aluno Aprovado')
+else:
+    print('Aluno Reprovado')
