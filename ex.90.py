cfcc9663ce4563bf650fe25e12edbe31a5d4ed33
Yago <yagoelorena@icloud.com>
2024-07-18 22:33:21 -0300

    Exercicio 90 Concluido

--- ex.90.py
+++ ex.90.py
@@ -4,5 +4,8 @@ situacao ['nota'] = int(input(f'Digite a media de {situacao["nome"]} : '))
 print(f'Média é igual a de {situacao["nome"]} é {situacao["nota"]} ')
 if situacao['nota']>=6:
     print(f'Aluno Aprovado')
+    situacao['final']= 'aprovado'
 else:
     print('Aluno Reprovado')
+    situacao['final']= 'Reprovado'
+print(situacao)
