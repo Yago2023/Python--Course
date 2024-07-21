9f64e8cd0286ef4a48b3d04dbf49a1b2ea114dcd
Yago <yagoelorena@icloud.com>
2024-07-21 14:51:56 -0300

    Exercicio

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,33 @@
-dados = dict() #criando um dicionario 
+'''dados = dict() #criando um dicionario 
 dados = {'nome': 'Pedro', 'idade': 25}
 print(dados)
 print(dados['nome'])
 print(dados['idade'])
 del dados['idade']
-filme = {'titulo' :'Star Wars', 'Ano': 1977 , 'Diretor':'George Lucas'}
+dados['peso'] = 99
+filme = {'titulo' :'Star Wars', 'Ano': 1977 , 'Diretor':'George Lucas'}'''
 '''print(filme.values()) #mostra todos os valores 
 print(filme.keys()) #mostra todas as Keys "numero de posições"
 print(filme.items()) #mostra todos os values and keys juntos '''
 
-for k,v in filme.items():
+'''for k,v in filme.items(): #items tenho que colocar key and value/caso não queira é só colocar .keys or .values
     print(f'O key {k} é valor:{v}')
+
+for k in filme.keys():
+    print(k)
+'''
+''' #colocar uma lista dentro de um dicionario 
+brasil = []
+estado1 = {'uf': 'Goias', 'sigla' : 'GO'}
+estado2 = {'uf': 'São Paulo', 'sigla' : 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil[1]['sigla'])
+'''
+estado = dict ()
+brasil = ()
+for c in range (0,3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+print(brasil)
--- ex.91.py
+++ ex.91.py
@@ -1,8 +1,21 @@
 from random import randint
-x = 1
-jogadores = dict()
-jog = []
-for c in range (4):
-    jogadores[f'jogador {c+1}: '] = randint (0, 10)
-    jog.append(jogadores.copy())
-print(jog)
+from time import sleep
+from operator import (itemgetter)
+jogo = {'jogador1': randint(1,6),
+        'jogador2': randint(1,6),
+        'jogador3': randint(1,6),
+        'jogador4': randint(1,6)}
+ranking = list()
+print('Valores sorteado: ')
+for k,v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(2)
+    ranking = (sorted(jogo.items(), key = itemgetter(1), reverse=True))
+for i,v in enumerate(ranking):
+    print(f'{i+1} lugar: {v[0]} com {v[1]}')
+    sleep(1)
+
+
+
+
+    
