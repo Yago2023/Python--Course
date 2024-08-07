cd63f40bd4472ba9a2138ee096ba548714c5ea91
Yago <yagoelorena@icloud.com>
2024-08-07 17:42:49 -0300

    Update

---
+++ ex.def_fun.py
@@ -0,0 +1,29 @@
+'''def soma (a,b):
+    s= a +b
+    print(f'A soma de {a} com {b} é igual a {s}')
+
+
+#Programa principal - deve pular 2 linhas 
+soma(6,5)
+soma(2,5)
+'''
+#empacotamento
+#def contador (*núm); #independente do tanto de parâmetro vai jogar tudo em num
+#	for valor in num:
+#		print(f'{valor} ', end='')
+#	print('FIM!')
+	
+'''
+def dobra(lst)
+	por = 0 
+	while pos<len(lst):
+	   lst[pos]*=2
+	pos +=1
+
+valores = [6,3,9,1,0,2]
+dobra (valores)
+print (valores)'''
+
+
+help()
+print(input.__doc__)
