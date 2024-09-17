8e75c4bef95be000d314b1304191c194f15c98a7
Yago <yagoelorena@icloud.com>
2024-09-17 15:48:17 -0300

    updates

--- Modulos/Number.py
+++ Modulos/Number.py
@@ -2,5 +2,7 @@ import uteis
 
 
 n = float(input('Digite o numero:  '))
-print(f'O dobro de {n} é {uteis.dobro(n)}')
-print(f'A metade de {n} é {uteis.metade(n)}')
+print(f'O dobro de {n} é R${uteis.dobro(n)}')
+print(f'A metade de {n} é R${uteis.metade(n)}')
+print(f' 10% desse valor é R${uteis.aumentar(n)}')
+print(f'13% desse valor é R$ {uteis.diminuir(n)}')
